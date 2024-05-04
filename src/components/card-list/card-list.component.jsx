40a9d5572fd2c951d400d81f754e0717340d3e39
Allen Joseph <allen.joseph@universum-inkasso.de>
2024-05-04 17:54:56 +0200

    Changed CardList and SearchBox to Functional Components

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,29 +1,27 @@
-import {Component} from "react";
 import './card-list.styles.css';
 
-class CardList extends Component {
-    render() {
-        const {monsters} = this.props;
+const CardList = ({monsters}) => {
 
-        return (
-            <div className='card-list'>
-                {
-                    monsters.map((monster) => {
-                        const {name, email, id} = monster;
-                        return (
-                            <div className='card-container' key={id}>
-                                <img
-                                    alt={`monster ${name}`}
-                                    src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
-                                <h1 className='name'>{name}</h1>
-                                <p>{email}</p>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+    return (
+        <div className='card-list'>
+            {
+                monsters.map((monster) => {
+                    const {name, email, id} = monster;
+                    return (
+                        <div className='card-container' key={id}>
+                            <img
+                                alt={`monster ${name}`}
+                                src={`https://robohash.org/${id}?set=set2&size=180x180`}/>
+                            <h1 className='name'>{name}</h1>
+                            <p>{email}</p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+
+
 }
 
 export default CardList;
