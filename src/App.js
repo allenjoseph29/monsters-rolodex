e25810be875292923c9fd11bd304dd6491027d9d
Allen Joseph <allen.joseph@universum-inkasso.de>
2024-05-04 15:58:28 +0200

    Added Card Containers and SearchBox

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import {Component} from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
     constructor() {
@@ -16,15 +18,11 @@ class App extends Component {
             .then((response) => response.json())
             .then((users) => this.setState(() => {
                     return {monsters: users};
-                },
-                () => {
-                    console.log(this.state);
                 }
             ));
     }
 
     onSearchChanged = (event) => {
-        console.log(event.target.value);
         const searchField = event.target.value.toLocaleLowerCase();
         this.setState(() => {
             return {searchField}
@@ -32,7 +30,6 @@ class App extends Component {
     };
 
     render() {
-
         const {monsters, searchField} = this.state;
         const {onSearchChanged} = this;
 
@@ -42,13 +39,9 @@ class App extends Component {
 
         return (
             <div className="App">
-                <input className='search-box' type='search' placeholder='search monsters'
-                       onChange={onSearchChanged}/>
-                {
-                    filteredMonsters.map((monster) => {
-                        return <div><h1>{monster.name}</h1></div>
-                    })
-                }
+                <h1 className='app-title'>Monsters Rolodex</h1>
+                <SearchBox onChangeHandler={onSearchChanged} placeholder='search monsters' className='search-box'/>
+                <CardList monsters={filteredMonsters}/>
             </div>
         );
     }
